f7ad5632f440764409c76e458dedbe7c73265eac
MohammadRakib1e18 <trustedrakib@gmail.com>
2021-11-15 09:24:46 +0600

    added server site

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.np0as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('carShop');
+        const productCollection = database.collection('products');
+
+        // GET API
+        app.get('/products', async(req, res)=>{
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+        // POST API
+        app.post('/products', async(req, res)=>{
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            console.log(result);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Hello Car Lovers!");
+});
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`);
+});
